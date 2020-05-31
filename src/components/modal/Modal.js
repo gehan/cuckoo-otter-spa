5e88e595d0c84672df6b51a2e1bc80aae8f4b723
RanjitSaimbi <ranjit.saimbi@gmail.com>
2020-05-31 20:19:21 +0100

    Add delete button styling

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import styles from './Modal.module'
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./Modal.module";
 
-const Modal = ({ isOpen, child }) =>
+const Modal = ({ isOpen, child, toggleModal }) =>
   isOpen
     ? ReactDOM.createPortal(
-      <>
-        <div className={styles.modalOverlay} />
-        <div className={styles.modalWrapper}>
-          <div className={styles.modal}>
-            <div className={styles.modalHeader} />
-            {child}
+        <>
+          <div className={styles.modalOverlay} />
+          <div className={styles.modalWrapper}>
+            <div className={styles.modal}>
+              <div className={styles.modalHeader}>
+                <button onClick={toggleModal}>
+                  <span>&times;</span>
+                </button>
+              </div>
+              {child}
+            </div>
           </div>
-        </div>
-      </>,
-      document.body
-    )
-    : null
+        </>,
+        document.body
+      )
+    : null;
 
-export default Modal
+export default Modal;
